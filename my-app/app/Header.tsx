d69ba3a916aaadb3ccedf7b8985ddf76390b5bbd
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-12-19 09:39:39 -0800

    created nav bar

--- my-app/app/Header.tsx
+++ my-app/app/Header.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 function Header() {
   return (
-    <header className='p-5 text'>
-      <p>This is the Header</p>
+    <header>
+      <nav className='bg-gray-800 p-4 flex justify-between items-center'>
+        <Link href='/' className='text-white font-bold text-2xl'>Personal Bartender</Link>
+        <div className='flex items-center'>
+          <Link href='/' className='text-white mr-4 hover:text-gray-400'>Home</Link>
+          <Link href='#' className='text-white mr-4 hover:text-gray-400'>About</Link>
+          <Link href='#' className='text-white mr-4 hover:text-gray-400'>Contact</Link>
+        </div>
+      </nav>
     </header>
   )
 }
