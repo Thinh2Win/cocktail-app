4870f409dccce297794171318750f1290e2007b3
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-12-19 14:08:55 -0800

    Implemented dotenv with next.js

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const dotenv = require('dotenv');
+
 module.exports = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
+  },
+  env: {
+    API_URL: process.env.API_URL,
   },
 };
