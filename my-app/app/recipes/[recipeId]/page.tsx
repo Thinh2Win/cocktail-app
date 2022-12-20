7048256821da19b0312db88fa26c1e3e0a29d808
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-12-19 17:15:19 -0800

    destructured data from fetch api call for readability

--- my-app/app/recipes/[recipeId]/page.tsx
+++ my-app/app/recipes/[recipeId]/page.tsx
@@ -9,9 +9,11 @@ type RecipeProps = {
 
 async function RecipePage({ params: { recipeId } }: RecipeProps) {
   let recipe = await fetchRecipesById(recipeId);
+  const {Name, Ingredients, Garnish, Preparation} = recipe[0];
+
   return (
     <div className="bg-yellow-300 rounded-lg shadow-lg p-4 mb-4">
-      <div className="text-2xl font-bold mb-2">{recipe[0].Name}</div>
+      <div className="text-2xl font-bold mb-2">{Name}</div>
       <div className="text-gray-700 font-serif mb-4">
         A recipe from my personal collection
       </div>
@@ -19,18 +21,18 @@ async function RecipePage({ params: { recipeId } }: RecipeProps) {
         <div className="w-1/3">
           <div className="text-lg font-bold mb-2">Ingredients:</div>
           <ul className="list-disc pl-4">
-            {recipe[0].Ingredients.split(',').map((ingredient, idx) => <li key={idx}>{ingredient}</li>)}
+            {Ingredients.split(',').map((ingredient, idx) => <li key={idx}>{ingredient}</li>)}
           </ul>
-            {recipe[0].Garnish &&
+            {Garnish &&
             <>
             <div className="text-lg font-bold mb-2">Garnish:</div>
-            {recipe[0].Garnish.split(',').map(ingredient => <li>{ingredient}</li>)}
+            {Garnish.split(',').map(ingredient => <li>{ingredient}</li>)}
             </>}
         </div>
         <div className="w-2/3">
           <div className="text-lg font-bold mb-2">Instructions:</div>
           <p className="list-decimal pl-4">
-            {recipe[0].Preparation}
+            {Preparation}
           </p>
         </div>
       </div>
