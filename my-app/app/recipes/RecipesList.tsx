e982acfcc87f40b2ce93b2d4df5d24066aa6fd83
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-12-19 17:14:24 -0800

    Refactored code for readability

--- my-app/app/recipes/RecipesList.tsx
+++ my-app/app/recipes/RecipesList.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { fetchRandomRecipe, fetchRecipesByName } from './recipes.api';
+import React from 'react';
 import Link from 'next/link';
+import { fetchRandomRecipe, fetchRecipesByName } from './recipes.api';
 
 async function RecipesList() {
   const recipes = await fetchRecipesByName('negroni');
@@ -8,17 +8,22 @@ async function RecipesList() {
   return (
     <div className="flex flex-wrap -mx-4 p-5">
       {recipes.map(recipe => {
-        return <div className="w-1/3 px-4 mb-8">
-          <div className="bg-yellow-100 rounded-lg shadow-lg p-4 mb-4">
-          <Link href={`/recipes/${recipe._id}`} className="text-1xl font-bold mb-2">{recipe.Name}</Link>
-          <p className="text-gray-700 font-serif mb-4">
-            A recipe from my personal collection
-          </p>
-          <img className="w-36 h-24 rounded-sm" src="https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"></img>
-        </div>
-    </div>
+        return <Recipe key={recipe._id} recipe={recipe}/>
     })}
-</div>
+  </div>
+  )
+}
+
+function Recipe({ recipe }: any) {
+  const {_id, Name} = recipe;
+
+  return (
+      <div className="w-1/3 px-4 mb-8">
+          <div className="bg-yellow-100 rounded-lg shadow-lg p-4 mb-4">
+          <Link href={`/recipes/${_id}`} className="text-1xl font-bold mb-2">{Name}</Link>
+          <img className="w-36 h-24 rounded-sm" src="https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"></img>
+          </div>
+        </div>
   )
 }
 
