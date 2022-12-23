43cc50a75853dfb3d6691a92529d6f9a875c8188
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-12-22 18:03:49 -0800

    implemented search by name, ingredients, and filtered ingredients

--- my-app/app/cocktails/search/Search.tsx
+++ my-app/app/cocktails/search/Search.tsx
@@ -5,30 +5,71 @@ import React, {FormEvent, useState} from 'react'
 
 function Search() {
   const [search, setSearch] = useState('');
+  const [select, setSelect] = useState('name');
   const router = useRouter();
 
   const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSearch('');
-    router.push(`/cocktails/search/${search}`)
+    switch(select) {
+      case 'name':
+        router.push(`/cocktails/search/name/${search}`);
+        break;
+      case 'ingredients':
+        router.push(`/cocktails/search/ingredients/${search}`);
+        break;
+      case 'filter':
+        router.push(`/cocktails/search/filter/${search}`);
+        break;
+    }
   }
 
   return (
-    <form onSubmit={handleSearch}>
-      <input
-        className='pr-5'
-        type='text'
-        value={search}
-        placeholder='cocktail name'
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button
-        type='submit'
-        className='bg-gray-400 text-white py-2 px-4 rounded-lg'
-      >
-        Search
-      </button>
-    </form>
+    <>
+      <form className='flex flex-row'>
+        <label className='block font-bold mr-2'>
+          <input
+            className='mr-2 leading-tight'
+            type='radio'
+            value='name'
+            checked={select === 'name'}
+            onChange={e => setSelect(e.target.value)}
+          /> Name </label>
+        <label className='block font-bold mr-2'>
+          <input
+            className='mr-2 leading-tight'
+            type='radio'
+            value='ingredients'
+            checked={select === 'ingredients'}
+            onChange={e => setSelect(e.target.value)}
+          /> By Ingredients </label>
+        <label className='block font-bold mr-2'>
+          <input
+            className='mr-2 leading-tight'
+            type='radio'
+            value='filter'
+            checked={select === 'filter'}
+            onChange={e => setSelect(e.target.value)}
+          /> Filter Ingredients
+        </label>
+      </form>
+
+      <form onSubmit={handleSearch}>
+        <input
+          className='pr-5'
+          type='text'
+          value={search}
+          placeholder='cocktail name'
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          type='submit'
+          className='bg-gray-400 text-white py-2 px-4 rounded-lg'
+        >
+          Search
+        </button>
+      </form>
+    </>
   )
 }
 
