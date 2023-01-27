b50b56261ac9e9b559ca5fd104572f14aeac9acc
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-01-26 16:41:40 -0800

    name page now works with updated cocktail data

--- my-app/app/cocktails/search/name/[name]/page.tsx
+++ my-app/app/cocktails/search/name/[name]/page.tsx
@@ -23,14 +23,14 @@ async function RecipesList({params: {name}}: RecipeProps) {
   )
 }
 
-function Recipe({ recipe, cocktails }: any) {
-  const {_id, Name} = recipe;
+function Recipe({ recipe }: any) {
+  const {_id, name, img} = recipe;
 
   return (
       <div className="w-1/3 px-4 mb-8">
           <div className="bg-yellow-600 rounded-lg shadow-lg p-4 mb-4">
-          <Link href={`/cocktails/${_id}`} className="text-1xl font-bold mb-2 text-gray-800">{Name}</Link>
-          <img className="w-36 h-24 rounded-md" src="https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"></img>
+          <Link href={`/cocktails/${_id}`} className="text-1xl font-bold mb-2 text-gray-800">{name}</Link>
+          <img className="w-36 h-24 rounded-md" src={img}></img>
           </div>
         </div>
   )
