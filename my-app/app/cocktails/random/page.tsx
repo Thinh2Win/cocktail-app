947b684c782b5fea7107ae9fa2eebdb7c714d984
Thinh Ngo Phan <ngophanthinh@gmail.com>
2023-01-26 16:39:38 -0800

    reroll button works

--- my-app/app/cocktails/random/page.tsx
+++ my-app/app/cocktails/random/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link';
 import { fetchRandomCocktail } from '../cocktails.api'
 
 async function getData() {
@@ -11,34 +12,34 @@ async function getData() {
 
 async function Random() {
   const data = await getData();
-  const {Name, Ingredients, Garnish, Preparation} = data[0];
-
+  const reroll = await fetchRandomCocktail();
+  const {name, ingredients, preparation, img} = data[0];
   return (
-      <div className="bg-yellow-600 rounded-lg shadow-lg p-4 m-10">
-        <div className="text-2xl font-bold mb-2">{Name}</div>
-        <div className="text-gray-700 font-serif mb-4">
-          A recipe from my personal collection
-        </div>
-        <div className="flex mb-4">
-          <div className="w-1/3">
-            <div className="text-lg font-bold mb-2">Ingredients:</div>
-            <ul className="list-disc pl-4">
-              {Ingredients.split(',').map((ingredient, idx) => <li key={idx}>{ingredient}</li>)}
-            </ul>
-              {Garnish &&
-              <>
-              <div className="text-lg font-bold pt-2 mb-2">Garnish:</div>
-              {Garnish.split(',').map(ingredient => <li>{ingredient}</li>)}
-              </>}
-          </div>
-          <div className="w-2/3">
-            <div className="text-lg font-bold mb-2">Instructions:</div>
-            <p className="list-decimal pl-4">
-              {Preparation}
-            </p>
-          </div>
-        </div>
+    <div className="bg-yellow-600 rounded-lg shadow-lg py-4 pl-4 m-10 max-w-lg mx-auto relative">
+    <span className="text-2xl font-bold mb-2">{name}</span>
+    <Link href={`/cocktails/random/${reroll[0]._id}`}>
+        <span className="absolute bg-black text-white pl-1 pr-1 right-2 rounded">♻︎ ☈e-☈oll</span>
+    </Link>
+    <div className="text-gray-700 font-serif mb-4">
+      A recipe from my personal collection
+    </div>
+    <div className="flex mb-4">
+      <div className="w-1/2">
+        <h3 className="text-lg font-bold mb-2">Ingredients:</h3>
+        <ul className="list-disc pl-4">
+          {ingredients.split(',').map((ingredient, idx) => <li key={idx}>{ingredient}</li>)}
+        </ul>
+        <h3 className="text-lg font-bold my-2">Instructions:</h3>
+        <p className="mr-2">
+          {preparation.split('.').filter(x => x !== '').map((steps, idx) => <li key={idx} className="list-decimal">{steps}</li>)}
+        </p>
       </div>
+
+      <div className="w-1/2 p-2">
+        <img className="h-64 rounded-lg" src={img}></img>
+      </div>
+    </div>
+  </div>
   )
 }
 
